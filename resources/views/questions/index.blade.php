7284e78c2201cbb8aa95ba8011640fd62809f909
Masud99Rana <Masudrana917775@gmail.com>
2019-11-29 20:22:02 +0600

    Question Controller, show all questions

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="card-body">
+                    {{-- @include ('layouts._messages') --}}
+
+                    @forelse ($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0"> {{$question->title }} </h3>
+                                {{ str_limit($question->body, $limit = 250, $end = '...') }}
+                            </div>
+                        </div>
+                        <hr>
+                    @empty
+                        <div class="alert alert-warning">
+                            <strong>Sorry</strong> There are no questions available.
+                        </div>
+                    @endforelse
+
+                        {{ $questions->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
