82342329b6166f4ef85968ae6e2ddb98a82aa5b0
Masud99Rana <Masudrana917775@gmail.com>
2019-11-29 19:18:10 +0600

    database configured, auth scaffolding, npm packages, Question model-migration-relationship

---
+++ app/Question.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title','body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+}
