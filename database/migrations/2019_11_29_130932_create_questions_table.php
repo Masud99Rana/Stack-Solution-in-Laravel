c14d752faa2a482bc30c49db0fada737b730dcac
Masud99Rana <Masudrana917775@gmail.com>
2019-11-29 19:40:35 +0600

    Question migration updated, fake data added using seeder

--- database/migrations/2019_11_29_130932_create_questions_table.php
+++ database/migrations/2019_11_29_130932_create_questions_table.php
@@ -20,8 +20,8 @@ class CreateQuestionsTable extends Migration
             $table->string('slug')->unique();
             $table->text('body');
             $table->unsignedInteger('views')->default(0);
-            $table->unsignedInteger('answers')->default(0);
-            $table->integer('votes')->default(0);
+            $table->unsignedInteger('answers_count')->default(0);
+            $table->integer('votes_count')->default(0);
             $table->unsignedInteger('best_answer_id')->nullable();
             $table->unsignedBigInteger('user_id');
             
